259e40ca17422fe47d6fae7c305985c116c341ed
JLer <jlerxky@live.com>
2025-11-13 09:44:45 +0800

    update

--- apply_secret.sh
+++ apply_secret.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl create secret tls web5 -n $NAMESPACE --key /etc/letsencrypt/live/$HOSTNAME/privkey.pem --cert /etc/letsencrypt/live/$HOSTNAME/fullchain.pem
+kubectl create secret tls web5 -n $NAMESPACE --key /etc/letsencrypt/live/res.bbsfans.dev/privkey.pem --cert /etc/letsencrypt/live/res.bbsfans.dev/fullchain.pem
